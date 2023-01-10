a15b1915408d1c2f5a93c84a6167627a545f70af
Liz Bailey <liz.bailey75@yahoo.com>
2023-01-09 20:18:06 -0500

    working file

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -14,4 +14,34 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.get('/', (req, res) => {
+  Comment.findAll()
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+router.delete('/:id', (req, res) => {
+  if (req.session) {
+      Comment.destroy({
+          where: {
+              id: req.params.id
+          }
+      })
+          .then(dbCommentData => {
+              if (!dbCommentData) {
+                  res.status(404).json({ message: 'No comment found with this id!' });
+                  return;
+              }
+              res.json(dbCommentData);
+          })
+          .catch(err => {
+              console.log(err);
+              res.status(500).json(err);
+          });
+  }
+});
+
 module.exports = router;
